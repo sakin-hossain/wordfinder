22f37559aca3cc824afeb8b8987f78283214996f
Md Sakin Hossain <info.sakinh@gmail.com>
2021-12-26 00:03:33 +0600

    finished

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      word: ' ',
+      vowel: 0,
+      consonent: 0,
+      char_length: 0,
+    }
+  }
+
+  Analyze = () => {
+    let word = this.state.word;
+    let consonent = 0;
+    let vowel = 0;
+    let char_length = 0;
+
+    char_length = word.length;
+
+    for (let i = 0; i <= word.length; i++) {
+
+      if (word[i] == 'a' || word[i] == 'u' || word[i] == 'i' || word[i] == 'o' || word[i] == 'e' || word[i] == 'A' || word[i] == 'U' || word[i] == 'I' || word[i] == 'O' || word[i] == 'E') {
+        vowel++;
+      }
+
+    }
+    consonent = word.length - vowel;
+
+    this.setState({ char_length: char_length, vowel: vowel, consonent: consonent });
+
+
+  }
+
+  render() {
+    return (
+      <View style={Styles.container}>
+        <View style={Styles.container}>
+          <Text style={Styles.welcome}>Word Finder</Text>
+        </View>
+        <View style={Styles.container}>
+          <TextInput onChangeText={(word) => this.setState({ word })} style={Styles.instructions} placeholder='Write the Word' />
+          <Text></Text>
+          <Text></Text>
+        </View>
+        <View style={Styles.container}>
+          <Button color="#FF8D33" onPress={this.Analyze} title='Analyze' />
+          <Text></Text>
+          <Text style={Styles.instructions}>Length of the word: {this.state.char_length}  </Text>
+          <Text style={Styles.instructions}>vowel: {this.state.vowel}  </Text>
+          <Text style={Styles.instructions}>Consonents: {this.state.consonent}  </Text>
+        </View>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
+const Styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#D1D1D1',
+  },
+  welcome: {
+    fontSize: 35,
+    textAlign: 'center',
+    margin: 10,
+    fontWeight: 'bold',
+    color: '#000000'
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#000000',
+    marginBottom: 5,
   },
 });
